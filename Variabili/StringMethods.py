45f840514da5ce7bb5d9e25aa03118bf88ad3d26
miki2000m <150129964+miki2000m@users.noreply.github.com>
2026-01-10 20:16:46 +0100

    Create StringMethods.py

---
+++ Variabili/StringMethods.py
@@ -0,0 +1,8 @@
+#Il programatore oltre a lavorare molto con i numeri e le operazioni matematiche, lavora anche con le stringhe.
+#Le stringhe sono sequenze di caratteri racchiuse tra virgolette singole (' ') o virgolette doppie (" ").
+#Python offre una vasta gamma di metodi per manipolare e lavorare con le stringhe in modo efficiente.
+
+nome=str(input("Inserisci il tuo nome: "))
+
+#Len -> conta quanti caratteri ci sono in una stringa
+print(f"La lunghezza del nome è {len(nome)}")
